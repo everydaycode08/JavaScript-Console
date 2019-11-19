0656e440a6d90667f20d18f381f9a3ba490b4636
Soumyajeet Nayak <soumyajeet08@gmail.com>
2019-11-19 23:07:50 +0530

    added required files and initial variables

---
+++ script.js
@@ -0,0 +1,9 @@
+const LOG_MESSAGE = 'message log';
+const WARN_MESSAGE = 'message warn';
+const ERROR_MESSAGE = 'message error';
+const TABLE_OBJECT = 
+    {
+        name: 'Mike',
+        age: 25,
+        designation: 'VP Marketing'
+    }
